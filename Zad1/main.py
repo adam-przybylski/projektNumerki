7367b6949e564c4cdafb1fad5ab772ba7ed873b4
242489 Pazio Jakub <242489@edu.p.lodz.pl>
2023-03-03 16:00:23 +0100

    Implemented taking input from user

--- Zad1/main.py
+++ Zad1/main.py
@@ -1,6 +1,34 @@
 import math
 from matplotlib import pyplot as plt
 
+class main:
+    if __name__ == "__main__":
+        func_count = input("Podaj ilość funkcji w twoim złożeniu: ")
+        lower_bound = input("Podaj dolny przedział poszukiwania miejsca zerowego: ")
+        upper_bound = input("Podaj górny przedział poszukiwania miejsca zerowego: ")
+        stop_criterion = input("Podaj warunek stopu: 1 - epsilon, 2 - ilośc iteracji: ")
+        if stop_criterion == "1":
+            epsilon = input("Podaj wartość epsilonu: ")
+        elif stop_criterion == "2":
+            iteration_number = input("Podaj ilość iteracji: ")
+        else:
+            exit(1)
+
+        user_functions = []
+
+        for i in  range(int(func_count)):
+            print('''Wybierz funkcje z których chcesz otrzymać złożenie:
+            1 - Wielomian
+            2 - Funkcje trygonometryczną
+            3 - Funkcje wykładniczą''')
+            user_fn = input()
+            user_functions.append(user_fn)
+
+        print(user_functions)
+# defined_functions = {
+#     "poli": 1,
+#     "trygon"
+# }
 
 def polynomial(x):
     x3 = x * x * x
