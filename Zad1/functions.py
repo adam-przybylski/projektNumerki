25c516a3c86f25389aea39b46aade2c72f3a5e48
Jayrondale <adamprzybylski789@gmail.com>
2023-03-05 20:15:14 +0100

    Sign condition doesn't work. Implemented horner

--- Zad1/functions.py
+++ Zad1/functions.py
@@ -1,12 +1,19 @@
 import math
 
 
-# zrobić średnią
-# schemat Hornera
+def horner(x, arguments):
+    result = 0
+    for coefficient in arguments:
+        result = result * x + coefficient
+    return result
+
+
+def avg(x1, x2):
+    return (x1 + x2) / 2
+
 
 def polynomial(x):
-    x2 = x ** 2
-    return x2 - x - 1
+    return horner(x, [1, -1, -1])
 
 
 def trigonometric(x):
@@ -19,7 +26,7 @@ def exponent_func(x):
 
 
 def polynomial_derivative(x):
-    return 2 * x - 1
+    return horner(x, [2, -1])
 
 
 def trigonometric_derivative(x):
@@ -33,40 +40,40 @@ def exponent_derivative(x):
 def bisection(f1, a, b, f2=None, epsilon=None, iteration_number=None):
     if f2 is not None:
         if epsilon is not None:
-            x1 = (a + b) / 2
+            x1 = avg(a, b)
             x1, a, b = bisection_algorithm(a, b, x1, f1(f2(a)), f1(f2(x1)))
-            x2 = (a + b) / 2
+            x2 = avg(a, b)
             x2, a, b = bisection_algorithm(a, b, x2, f1(f2(a)), f1(f2(x2)))
             i = 2
             while abs(x2 - x1) >= epsilon:
                 x1 = x2
-                x2 = (a + b) / 2
+                x2 = avg(a, b)
                 x2, a, b = bisection_algorithm(a, b, x2, f1(f2(a)), f1(f2(x2)))
                 i += 1
             return x2, i
         else:
             x1 = None
             for i in range(iteration_number):
-                x1 = (a + b) / 2
+                x1 = avg(a, b)
                 x1, a, b = bisection_algorithm(a, b, x1, f1(f2(a)), f1(f2(x1)))
             return x1
     else:
         if epsilon is not None:
-            x1 = (a + b) / 2
+            x1 = avg(a, b)
             x1, a, b = bisection_algorithm(a, b, x1, f1(a), f1(x1))
-            x2 = (a + b) / 2
+            x2 = avg(a, b)
             x2, a, b = bisection_algorithm(a, b, x2, f1(a), f1(x2))
             i = 2
             while abs(x2 - x1) >= epsilon:
                 x1 = x2
-                x2 = (a + b) / 2
+                x2 = avg(a, b)
                 x2, a, b = bisection_algorithm(a, b, x2, f1(a), f1(x2))
                 i += 1
             return x2, i
         else:
             x1 = None
             for i in range(iteration_number):
-                x1 = (a + b) / 2
+                x1 = avg(a, b)
                 x1, a, b = bisection_algorithm(a, b, x1, f1(a), f1(x1))
             return x1
 
@@ -80,7 +87,7 @@ def bisection_algorithm(a, b, x1, func1, func2):
 
 
 def newton_method(f1, fp1, a, b, f2=None, fp2=None, epsilon=None, iteration_number=None):
-    x1 = (a + b) / 2
+    x1 = avg(a, b)
     x2 = x1 - f1(x1) / fp1(x1)
     if f2 is not None:
         if epsilon is not None:
--- Zad1/main.py
+++ Zad1/main.py
@@ -17,8 +17,11 @@ class Main:
     epsilon = None
     iteration_number = None
     user_functions = [None, None]
+    fp1 = None
+    fp2 = None
 
     if __name__ == "__main__":
+
         func_count = input("Podaj ilość funkcji w twoim złożeniu: ")
         for i in range(int(func_count)):
             print('''Wybierz funkcje z których chcesz otrzymać złożenie:
@@ -27,6 +30,7 @@ class Main:
             3 - Funkcje wykładniczą''')
             user_fn = input()
             user_functions[i] = user_fn
+
         a = float(input("Podaj dolny przedział poszukiwania miejsca zerowego: "))
         b = float(input("Podaj górny przedział poszukiwania miejsca zerowego: "))
 
@@ -37,12 +41,23 @@ class Main:
             iteration_number = int(input("Podaj ilość iteracji: "))
         else:
             print("Złe kryterium")
-            exit(1)
+            exit(74)
 
         f2 = defined_functions[user_functions[1]]
         f1 = defined_functions[user_functions[0]]
-        fp1 = defined_functions[2 * user_functions[0]]
-        fp2 = defined_functions[2 * user_functions[1]]
+        if user_functions[0] is not None:
+            fp1 = defined_functions[2 * user_functions[0]]
+        if user_functions[1] is not None:
+            fp2 = defined_functions[2 * user_functions[1]]
+
+        if fp2 is not None:
+            if (fp1(fp2(a)) < 0 and fp1(fp2(b)) < 0) or fp1(fp2(a)) > 0 and fp1(fp2(b)) > 0:
+                print("Funkcja na krańcach przedziału musi mieć różne znaki")
+                exit(74)
+        else:
+            if fp1(a) < 0 and fp1(b) < 0 or fp1(a) > 0 and fp1(b) > 0:
+                print("Funkcja na krańcach przedziału musi mieć różne znaki")
+                exit(74)
 
         print(f'Metoda bisekcji: {f.bisection(f1, a, b, epsilon=epsilon, iteration_number=iteration_number, f2=f2)}')
         print(
