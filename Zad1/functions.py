a90c284a20e17934d8afc1935577fefc01c73b6d
Jayrondale <adamprzybylski789@gmail.com>
2023-03-06 23:39:20 +0100

    Implemeted plotting a function and function that returns derivative value???

--- Zad1/functions.py
+++ Zad1/functions.py
@@ -1,14 +1,5 @@
 import math
-
-
-def nested_function(f1, x, f2=None, f3=None):
-    if f2 is None:
-        return f1(x)
-    if f3 is None:
-        y = f1(f2(x))
-        return y
-    else:
-        return f1(f2(f3(x)))
+import numpy as np
 
 
 def horner(x, arguments):
@@ -27,7 +18,7 @@ def polynomial(x):
 
 
 def trigonometric(x):
-    return math.cos(x)
+    return np.cos(x)
 
 
 def exponent_func(x):
@@ -40,52 +31,20 @@ def polynomial_derivative(x):
 
 
 def trigonometric_derivative(x):
-    return -math.sin(x)
+    return -np.sin(x)
 
 
 def exponent_derivative(x):
     return math.log(2, math.e) * 2 ** x
 
 
-def bisection(f1, a, b, f2=None, epsilon=None, iteration_number=None):
-    if f2 is not None:
-        if epsilon is not None:
-            x1 = avg(a, b)
-            x1, a, b = bisection_algorithm(a, b, x1, f1(f2(a)), f1(f2(x1)))
-            x2 = avg(a, b)
-            x2, a, b = bisection_algorithm(a, b, x2, f1(f2(a)), f1(f2(x2)))
-            i = 2
-            while abs(x2 - x1) >= epsilon:
-                x1 = x2
-                x2 = avg(a, b)
-                x2, a, b = bisection_algorithm(a, b, x2, f1(f2(a)), f1(f2(x2)))
-                i += 1
-            return x2, i
-        else:
-            x1 = None
-            for i in range(iteration_number):
-                x1 = avg(a, b)
-                x1, a, b = bisection_algorithm(a, b, x1, f1(f2(a)), f1(f2(x1)))
-            return x1
+def nested_function(f1, x, f2=None, f3=None):
+    if f2 is None:
+        return f1(x)
+    if f3 is None:
+        return f1(f2(x))
     else:
-        if epsilon is not None:
-            x1 = avg(a, b)
-            x1, a, b = bisection_algorithm(a, b, x1, f1(a), f1(x1))
-            x2 = avg(a, b)
-            x2, a, b = bisection_algorithm(a, b, x2, f1(a), f1(x2))
-            i = 2
-            while abs(x2 - x1) >= epsilon:
-                x1 = x2
-                x2 = avg(a, b)
-                x2, a, b = bisection_algorithm(a, b, x2, f1(a), f1(x2))
-                i += 1
-            return x2, i
-        else:
-            x1 = None
-            for i in range(iteration_number):
-                x1 = avg(a, b)
-                x1, a, b = bisection_algorithm(a, b, x1, f1(a), f1(x1))
-            return x1
+        return f1(f2(f3(x)))
 
 
 def bisection_algorithm(a, b, x1, func1, func2):
@@ -93,35 +52,63 @@ def bisection_algorithm(a, b, x1, func1, func2):
         b = x1
     else:
         a = x1
-    return x1, a, b
+    return a, b
 
 
-def newton_method(f1, fp1, a, b, f2=None, fp2=None, epsilon=None, iteration_number=None):
-    x1 = avg(a, b)
-    x2 = x1 - f1(x1) / fp1(x1)
-    if f2 is not None:
-        if epsilon is not None:
-            i = 2
-            while abs(x2 - x1) >= epsilon:
-                x1 = x2
-                x2 = x1 - f1(f2(x1)) / (fp1(f2(x1)) * fp2(x1))
-                i += 1
-            return x2, i
-        else:
-            for i in range(2, iteration_number):
-                x1 = x2
-                x2 = x1 - f1(f2(x1)) / (fp1(f2(x1)) * fp2(x1))
-            return x2
+def bisection(f1, a, b, f2=None, f3=None, epsilon=None, iteration_number=None):
+    if epsilon is not None:
+        x1 = avg(a, b)
+        a, b = bisection_algorithm(a, b, x1, nested_function(f1, a, f2, f3), nested_function(f1, x1, f2, f3))
+        x2 = avg(a, b)
+        a, b = bisection_algorithm(a, b, x2, nested_function(f1, a, f2, f3), nested_function(f1, x2, f2, f3))
+        i = 2
+        while abs(x2 - x1) >= epsilon:
+            x1 = x2
+            x2 = avg(a, b)
+            a, b = bisection_algorithm(a, b, x2, nested_function(f1, a, f2, f3), nested_function(f1, x2, f2, f3))
+            i += 1
+        return x2, i
     else:
-        if epsilon is not None:
-            i = 2
-            while abs(x2 - x1) >= epsilon:
-                x1 = x2
-                x2 = x1 - f1(x1) / fp1(x1)
-                i += 1
-            return x2, i
-        else:
-            for i in range(2, iteration_number):
-                x1 = x2
-                x2 = x1 - f1(x1) / fp1(x1)
-            return x2
+        x1 = None
+        for i in range(iteration_number):
+            x1 = avg(a, b)
+            a, b = bisection_algorithm(a, b, x1, nested_function(f1, a, f2, f3), nested_function(f1, x1, f2, f3))
+        return x1
+
+
+def nested_function_derivative(fp1, x, fp2=None, fp3=None, f2=None, f3=None):
+    if fp2 is None:
+        return fp1(x)
+    if fp3 is None:
+        return fp1(f2(x)) * fp2(x)
+    else:
+        return fp1(f2(f3(x))) * fp2(f3(x)) * fp3(x)
+
+
+def newton_method(f1, fp1, a, b, f2=None, fp2=None, f3=None, fp3=None, epsilon=None, iteration_number=None):
+    x1 = avg(a, b)
+    derivative = nested_function_derivative(fp1, x1, fp2, fp3, f2, f3)
+    if derivative == 0:
+        print("Nie można dokończyć algorytmu bo jedna z pochodnych jest równa 0")
+        return
+    x2 = x1 - nested_function(f1, x1, f2, f3) / derivative
+    if epsilon is not None:
+        i = 2
+        while abs(x2 - x1) >= epsilon:
+            x1 = x2
+            derivative = nested_function_derivative(fp1, x1, fp2, fp3, f2, f3)
+            if derivative == 0:
+                print("Nie można dokończyć algorytmu bo jedna z pochodnych jest równa 0")
+                return
+            x2 = x1 - nested_function(f1, x1, f2, f3) / derivative
+            i += 1
+        return x2, i
+    else:
+        for i in range(2, iteration_number):
+            x1 = x2
+            derivative = nested_function_derivative(fp1, x1, fp2, fp3, f2, f3)
+            if derivative == 0:
+                print("Nie można dokończyć algorytmu bo jedna z pochodnych jest równa 0")
+                return
+            x2 = x1 - nested_function(f1, x1, f2, f3) / derivative
+        return x2
--- Zad1/main.py
+++ Zad1/main.py
@@ -1,4 +1,4 @@
-import math
+import numpy as np
 from matplotlib import pyplot as plt
 import functions as f
 
@@ -16,19 +16,17 @@ class Main:
 
     epsilon = None
     iteration_number = None
-    user_functions = [None, None]
+    user_functions = [None, None, None]
     fp1 = None
     fp2 = None
+    fp3 = None
 
     if __name__ == "__main__":
-        f1 = f.polynomial
-        f2 = f.trigonometric
-        f3 = f.exponent_func
-        print(f.nested_function(f1, 2))
-        print(f.nested_function(f1, 2, f2))
-        print(f.nested_function(f1, 2, f2, f3))
-        func_count = input("Podaj ilość funkcji w twoim złożeniu: ")
-        for i in range(int(func_count)):
+        func_count = int(input("Podaj liczbę funkcji w twoim złożeniu (max 3): "))
+        if func_count > 3:
+            print("Przekroczono limit funkcji do złożenia.")
+            exit(1)
+        for i in range(func_count):
             print('''Wybierz funkcje z których chcesz otrzymać złożenie:
             1 - Wielomian
             2 - Funkcje trygonometryczną
@@ -46,24 +44,36 @@ class Main:
             iteration_number = int(input("Podaj ilość iteracji: "))
         else:
             print("Zły warunek stopu")
-            exit(74)
+            exit(1)
 
+        f3 = defined_functions[user_functions[2]]
         f2 = defined_functions[user_functions[1]]
         f1 = defined_functions[user_functions[0]]
         if user_functions[0] is not None:
             fp1 = defined_functions[2 * user_functions[0]]
         if user_functions[1] is not None:
             fp2 = defined_functions[2 * user_functions[1]]
+        if user_functions[2] is not None:
+            fp3 = defined_functions[2 * user_functions[2]]
 
-        if fp2 is not None:
-            if (f1(f2(a)) < 0 and f1(f2(b)) < 0) or (f1(f2(a)) > 0 and f1(f2(b)) > 0):
-                print("Funkcja na krańcach przedziału musi mieć różne znaki")
-                exit(74)
+        if (f.nested_function(f1, a, f2, f3) < 0 and f.nested_function(f1, b, f2, f3) < 0) or (
+                f.nested_function(f1, a, f2, f3) > 0 and f.nested_function(f1, b, f2, f3) > 0):
+            print(
+                "Aby wykonać poszukiwanie miejsca zerowego metodą bisekcji funkcja na krańcach przedziału musi mieć "
+                "różne znaki")
         else:
-            if (f1(a) < 0 and f1(b) < 0) or (f1(a) > 0 and f1(b) > 0):
-                print("Funkcja na krańcach przedziału musi mieć różne znaki")
-                exit(74)
+            print(
+                f'Metoda bisekcji: {f.bisection(f1, a, b, epsilon=epsilon, iteration_number=iteration_number, f2=f2, f3=f3)}')
 
-        print(f'Metoda bisekcji: {f.bisection(f1, a, b, epsilon=epsilon, iteration_number=iteration_number, f2=f2)}')
         print(
-            f'Metoda stycznych: {f.newton_method(f1, fp1, a, b, epsilon=epsilon, iteration_number=iteration_number, f2=f2, fp2=fp2)}')
+            f'Metoda stycznych: {f.newton_method(f1, fp1, a, b, epsilon=epsilon, iteration_number=iteration_number, f2=f2, fp2=fp2, f3=f3, fp3=fp3)}')
+
+        # plt.rcParams["figure.figsize"] = [7.50, 7.50]
+        # plt.rcParams["figure.autolayout"] = True
+
+        xlist = np.linspace(-5, 5, num=1000)
+        # xlist = np.arange(-10, 10.1, .1, )
+
+        plt.figure(num=0, dpi=120)
+        plt.plot(xlist, f.nested_function(f1, xlist, f2, f3), color='red')
+        plt.show()
