791ec64cf9ad605b7b3f44d65b08b0d06d43526a
adam-przybylski <adamprzybylski789@gmail.com>
2023-05-13 23:27:56 +0200

    A little bit of refactoring (trolling)

--- Zad4/functions.py
+++ Zad4/functions.py
@@ -3,6 +3,7 @@ import numpy as np
 poly_args = []
 a_linear = 0
 b_linear = 0
+exponent_base = 0
 
 
 def horner(x, args):
@@ -10,6 +11,10 @@ def horner(x, args):
     for coefficient in args:
         result = result * x + coefficient
     return result
+
+
+def exponent(x):
+    return exponent_base ** x
 
 
 def polynomial(x):
@@ -44,58 +49,68 @@ def weight_function(x):
     return 1 / np.sqrt(1 - x ** 2)
 
 
-def interp(f, n, a, b, xi):
+def interp(f1, f2, f3, a, b, xi, ver):
+    if len(poly_args) < 5:
+        n = 5
+    else:
+        n = len(poly_args) + 1
     interp_nodes_x = np.linspace(a, b, num=n)
-    interp_nodes_y = f(interp_nodes_x)
+    if ver == 1:
+        interp_nodes_y = nested_function(f1, interp_nodes_x, f2, f3) * weight_function(interp_nodes_x)
+    else:
+        interp_nodes_y = nested_function(f1, interp_nodes_x, f2, f3)
     yi = np.zeros(xi.size)
 
     for i in range(xi.size):
         yi[i] = lagrange_interp(interp_nodes_x, interp_nodes_y, xi[i])
-    return xi, yi
+    return yi
 
 
-def simpson(f, a, b, tol):
+def simpson(f1, f2, f3, a, b, tol):
     n = 2
     h = (b - a) / n
     x = np.linspace(a, b, n + 1)
-    y = f(x) * weight_function(x)
+    y = interp(f1, f2, f3, a, b, x, 1)
+    # y = f(x) * weight_function(x)
     S = h / 3 * np.sum(y[0:-1:2] + 4 * y[1::2] + y[2::2])
     err = np.inf
     while err > tol:
         n *= 2
         h = (b - a) / n
         x = np.linspace(a, b, n + 1)
-        y = f(x) * weight_function(x)
+        y = interp(f1, f2, f3, a, b, x, 1)
+        # y = f(x) * weight_function(x)
         S_new = h / 3 * np.sum(y[0:-1:2] + 4 * y[1::2] + y[2::2])
         err = abs(S - S_new)
         S = S_new
     return S
 
 
-def simpson_with_limit(f, tol):
+def simpson_with_limit(f1, f2, f3, tol):
     a = 0
     b = 0.5
-    temp = simpson(f, a, b, tol)
+    temp = simpson(f1, f2, f3, a, b, tol)
     S1 = temp
     while abs(temp) > tol:
         a = b
         b = b + (1 - b) / 2
-        temp = simpson(f, a, b, tol)
+        temp = simpson(f1, f2, f3, a, b, tol)
         S1 += temp
 
     a = -0.5
     b = 0
-    temp = simpson(f, a, b, tol)
+    temp = simpson(f1, f2, f3, a, b, tol)
     S1 += temp
     while abs(temp) > tol:
         b = a
         a = a - (1 + a) / 2
-        temp = simpson(f, a, b, tol)
+        temp = simpson(f1, f2, f3, a, b, tol)
         S1 += temp
 
     return S1
 
 
-def gauss_chebyshev(f, n):
+def gauss_chebyshev(f1, f2, f3, n):
     x, w = np.polynomial.chebyshev.chebgauss(n)
-    return sum(w * f(x))
+    y = interp(f1, f2, f3, -1, 1, x, 2)
+    return sum(w * y)
--- Zad4/main.py
+++ Zad4/main.py
@@ -4,40 +4,63 @@ import time
 
 
 def main():
+    defined_functions = {
+        '1': np.sin,
+        '2': np.tan,
+    }
     global epsilon
-    func = None
+    user_functions = [None, None, None]
     print("Witaj w programie do całkowania numerycznego!")
     epsilon = float(input("Podaj dokładność dla metody Simpsona: "))
-    print("""Jaka funkcja wariacie?
-                1: Liniowa
-                2: Wielomianowa
-                3: Sinus
-                4: Cosinus""")
-    fn_number = input()
-    match fn_number:
-        case "1":
-            f.a_linear = float(input("Podaj parametr a: "))
-            f.b_linear = float(input("Podaj parametr b: "))
-            func = f.linear
-        case "2":
-            n = int(input("Podaj stopien wielomianu: "))
-            for i in range(n + 1):
-                f.poly_args.append(float(input(f'Podaj wspolczynnik nr {i + 1}: ')))
-            func = f.polynomial
-        case "3":
-            func = np.sin
-            return
-        case "4":
-            func = np.cos
-            return
+    func_count = int(input("Podaj liczbę funkcji w twoim złożeniu (max 3): "))
+    if func_count > 3:
+        print("Przekroczono limit funkcji do złożenia.")
+        exit(1)
+    for i in range(func_count):
+        print('''Wybierz funkcje z których chcesz otrzymać złożenie:
+                    1 - Wielomian
+                    2 - Funkcje trygonometryczną
+                    3 - Funkcje liniową
+                    4 - Funkcja wykładnicza''')
+        func_type = input()
+        if func_type == '1':
+            degree = int(input("Podaj stopień wieolomianu: "))
+            print("Podaj współczynniki wielomianu (od najwyższego do najniższego)")
+            for j in range(degree + 1):
+                a = float(input(f'Podaj {j + 1} współczynnik: '))
+                f.poly_args.append(a)
+            user_functions[i] = f.polynomial
+        elif func_type == '2':
+            print('''Wybierz funkcje trygonometryczną:
+                                    1 - sin(x)
+                                    2 - tg(x)''')
+            user_choice = input()
+            if user_choice not in defined_functions.keys():
+                print("Nie ma takiego wyboru.")
+                exit(-1)
+            user_functions[i] = defined_functions[user_choice]
+        elif func_type == '3':
+            f.a_linear = float(input("Wybierz współczynnik 'a' funkcji liniowej: "))
+            f.b_linear = float(input("Wybierz współczynnik 'b' funkcji liniowej: "))
+            user_functions[i] = f.linear
+        elif func_type == '4':
+            f.exponent_base = float(input("Wybierz podstawę funkcji wykładniczej: "))
+            user_functions[i] = f.exponent
+        else:
+            print("Wybrano złą funkcję.")
+            exit(-1)
 
-    S = f.simpson_with_limit(func, epsilon)
+    f3 = user_functions[2]
+    f2 = user_functions[1]
+    f1 = user_functions[0]
+
+    S = f.simpson_with_limit(f1, f2, f3, epsilon)
     print(f'Metoda Simpsona: {S}')
     print("Metoda Gaussa-Czebyszewa:")
-    print(f'    2 węzły: {f.gauss_chebyshev(func, 2)}')
-    print(f'    3 węzły: {f.gauss_chebyshev(func, 3)}')
-    print(f'    4 węzły: {f.gauss_chebyshev(func, 4)}')
-    print(f'   5 węzłów: {f.gauss_chebyshev(func, 5)}')
+    print(f'    2 węzły: {f.gauss_chebyshev(f1, f2, f3, 2)}')
+    print(f'    3 węzły: {f.gauss_chebyshev(f1, f2, f3, 3)}')
+    print(f'    4 węzły: {f.gauss_chebyshev(f1, f2, f3, 4)}')
+    print(f'   5 węzłów: {f.gauss_chebyshev(f1, f2, f3, 5)}')
     return
 
 
