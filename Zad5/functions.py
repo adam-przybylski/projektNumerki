67c30506bfad93a1d4f33e09bd48767593d56249
Jayrondale <adamprzybylski789@gmail.com>
2023-05-22 14:40:08 +0200

    Minor changes

--- Zad5/functions.py
+++ Zad5/functions.py
@@ -38,8 +38,8 @@ def simpson(f, a, b, n):
     h = (b - a) / n
     x = np.linspace(a, b, n + 1)
     y = f(x)
-    S = h / 3 * np.sum(y[0:-1:2] + 4 * y[1::2] + y[2::2])
-    return S
+    s = h / 3 * np.sum(y[0:-1:2] + 4 * y[1::2] + y[2::2])
+    return s
 
 
 def gauss_chebyshev(f, n):
